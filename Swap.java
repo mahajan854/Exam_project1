1dfcb3a0dcee653fb4bbe087244acb2a4fbe26c3
mahajan854 <mahajannilesh854@gmail.com>
2023-05-05 10:27:34 +0530

    done

---
+++ Swap.java
@@ -0,0 +1,16 @@
+
+
+
+ class Ques7 {
+	public static void main(String[]args) {
+		int num1=5;
+	      int num2=6;
+		System.out.println("before swaping:"+num1+" "+num2);
+		 num1=num1+num2;
+		 num2=num1-num2;
+		 num1=num1-num2;
+		 System.out.println("After swapping:"+num1+" "+num2);
+	
+	}
+
+}
