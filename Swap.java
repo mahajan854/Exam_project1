e492edddf8ffb5467043c2f1399eb7f080b9e38e
mahajan854 <97273501+mahajan854@users.noreply.github.com>
2023-05-05 10:46:23 +0530

    Update Swap.java

--- Swap.java
+++ Swap.java
@@ -4,13 +4,14 @@
  class Ques7 {
 	public static void main(String[]args) {
 		int num1=5;
-	      int num2=6;
+	        int num2=6;
+		int num3=num1+num2;
 		System.out.println("before swaping:"+num1+" "+num2);
 		 num1=num1+num2;
 		 num2=num1-num2;
 		 num1=num1-num2;
 		 System.out.println("After swapping:"+num1+" "+num2);
-	
+	        System.out.println("Add:"+num3);
 	}
 
 }
